16c090f4bbe08a98535a3086a27531a34eecb900
Nadine <nadlgit@users.noreply.github.com>
2023-06-11 15:57:14 -0400

    chore: project structure

---
+++ app-entry.ts
@@ -0,0 +1,7 @@
+// Original file: node_modules/expo/AppEntry.js
+
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import App from '@/app';
+
+registerRootComponent(App);
